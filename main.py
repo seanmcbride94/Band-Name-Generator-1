9f244d73c63357641fd2deb6a74e6c8ddf2208ab
s3anmc94 <s3anmc94@hotmail.co.uk>
2022-07-21 11:18:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator")
+print("What city did you grow up in?")
+city = input()
+print("What was the name of your first pet?")
+pet = input()
+print("Your ideal band name is: " + city + " " + pet)
